413b2562c8b353f48711a6ab0b6cb5e8c3fbd189
jean-knapp <email+github@jeankn.app>
2023-07-05 16:59:27 -0300

    WIP - can't remember what I did here, sorry.

---
+++ game/source2mod_template/_vrad3/lightmap.cmd
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000000.dat -blocknum=0 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000001.dat -blocknum=1 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000002.dat -blocknum=2 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000003.dat -blocknum=3 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000004.dat -blocknum=4 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000005.dat -blocknum=5 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000006.dat -blocknum=6 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000007.dat -blocknum=7 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000008.dat -blocknum=8 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000009.dat -blocknum=9 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000010.dat -blocknum=10 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000011.dat -blocknum=11 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000012.dat -blocknum=12 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000013.dat -blocknum=13 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000014.dat -blocknum=14 -threads 6 -outdir results/outputs
+vrad3 -map maps/cs2_test1.vmap -script script.vrad3 -entrypoint lm_compute -blockfile=lm_block_00000015.dat -blocknum=15 -threads 6 -outdir results/outputs
